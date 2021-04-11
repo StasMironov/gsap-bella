c4a91b5650616a62ea1649a33f02d7256a880862
Mironov Stas <stasmironovweb@gmail.com>
2021-04-11 15:41:33 +0300

    animated navlink

--- l1/00-Full-Page/js/main.js
+++ l1/00-Full-Page/js/main.js
@@ -3,6 +3,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 function initNavigation(){
     const mainNavLinks = gsap.utils.toArray('.main-nav a');
+    const mainNavLinksRev = gsap.utils.toArray('.main-nav a').reverse();
 
     mainNavLinks.forEach(link => {
         link.addEventListener('mouseleave', e => {
@@ -16,7 +17,39 @@ function initNavigation(){
 
         });
     });
+
+    function navAnimation(direction){
+        const scrollingDown = direction === 1;
+        const links = scrollingDown ? mainNavLinks : mainNavLinksRev; 
+        return gsap.to(
+             links,
+             {
+                 duration: 0.3,
+                 stagger: 0.05,
+                 autoAlpha: () => scrollingDown ? 0 : 1,
+                 y: () => scrollingDown ? 20: 0,
+                 ease: 'Power4.out'
+             }
+         );
+     }
+
+     ScrollTrigger.create({
+        start: 100, 
+        end: 'bottom bottom-=20',
+        toggleClass: {
+            targets: 'body',
+            className: 'has-scrolled'
+        },
+        onEnter: ({direction})=>navAnimation(direction),
+        onLeaveBack: ({direction})=>navAnimation(direction),
+        markers: true
+    
+    });
 }
+
+
+
+
 
 function init(){
     
