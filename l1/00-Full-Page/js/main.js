8ec1dc1431b21002c196522c9b5c7403a6662cd8
Mironov Stas <stasmironovweb@gmail.com>
2021-04-11 10:51:25 +0300

    lesson 1

---
+++ l1/00-Full-Page/js/main.js
@@ -0,0 +1,29 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+function initNavigation(){
+    const mainNavLinks = gsap.utils.toArray('.main-nav a');
+
+    mainNavLinks.forEach(link => {
+        link.addEventListener('mouseleave', e => {
+            // add class
+            link.classList.add('animate-out');
+
+            setTimeout(()=>{
+                // remove class
+                link.classList.remove('animate-out');
+            }, 300);
+
+        });
+    });
+}
+
+function init(){
+    
+   initNavigation();
+
+}
+
+window.addEventListener('load', function(){
+    init();
+});
